c5826705681eb7de3035eedc7c71b62037fe5cdf
XinCaiTen <duannguyen09091@gmail.com>
2025-12-12 17:06:55 +0700

    first commit

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase Configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDFoGxmHkMit1tzOvprjoSFsfQyWYTMAiU",
+  authDomain: "diem-danh-thu-7.firebaseapp.com",
+  projectId: "diem-danh-thu-7",
+  storageBucket: "diem-danh-thu-7.firebasestorage.app",
+  messagingSenderId: "1037664124634",
+  appId: "1:1037664124634:web:f0948b0776686a19f96fe3",
+  measurementId: "G-5T9206YLY6"
+};
